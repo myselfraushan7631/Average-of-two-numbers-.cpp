8c406fa500e41f9606dc65c8bb1f36bd387b945c
Ravi Raushan <131746306+myselfraushan7631@users.noreply.github.com>
2023-06-30 15:17:39 +0530

    Add files via upload

---
+++ C++ programing/2nd .cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std ;
+main ()
+{
+    cout<<"Raushan\n" ;
+    cout<<"Akhilesh\n" ;
+}
+
+
+#include<iostream>
+using namespace std ;
+int num = 100 ;
+main ()
+{
+    int num = 10 ;
+    cout <<:: num ;
+}
+
+// average of two numbers
+ #include<iostream>
+using namespace std ;
+main ()
+{
+    float number1,number2,sum,average ;
+    cout<<"Enter two number:";
+cin >> number1 ;
+cin >> number2 ; 
+    sum =number1+number2;
+    average =sum/2;
+    cout <<"sum=\n";
+cout <<"average=\n";
+return 0 ;
+}
+
